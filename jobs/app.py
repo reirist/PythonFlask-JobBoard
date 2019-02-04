6566fd72a1deb2d7474e2bed7d1badbe83b71cbe
reirist <46998804+reirist@users.noreply.github.com>
2019-02-03 20:59:55 -0500

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, g
 import sqlite3
+from flask import Flask, render_template, g
 
 PATH = 'db/jobs.sqlite'
 
@@ -10,7 +10,7 @@ def open_connection():
     if connection == None:
         connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
-    return conenction
+    return connection
 
 def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
@@ -18,7 +18,7 @@ def execute_sql(sql, values=(), commit=False, single=False):
     if commit == True:
         results = connection.commit()
     else:
-        results = cursor.fectone() if single else cursor.fetchall()
+        results = cursor.fecthone() if single else cursor.fetchall()
 
     cursor.close()
     return results
